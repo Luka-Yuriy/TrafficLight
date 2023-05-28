9009324f37a1933b3aad1b53b9d4e77fa1211153
Luka Yurii <lukayurii.work@gmail.com>
2023-05-28 17:42:56 +0500

    Setup View

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentColor {
+    case red
+    case yellow
+    case green
+}
 
+class ViewController: UIViewController {
+    @IBOutlet var redSection: UIView!
+    @IBOutlet var yellowSection: UIView!
+    @IBOutlet var greenSection: UIView!
+    
+    @IBOutlet var colorChangeButton: UIButton!
+    
+    private var currentColor = CurrentColor.red
+    private let lightIsON: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorChangeButton.layer.cornerRadius = 12
+        
+        redSection.layer.cornerRadius = 60
+        yellowSection.layer.cornerRadius = 60
+        greenSection.layer.cornerRadius = 60
+        
+        redSection.alpha = lightIsOff
+        yellowSection.alpha = lightIsOff
+        greenSection.alpha = lightIsOff
+    
     }
 
-
+    @IBAction func colorChangeButtonDidTapped() {
+        colorChangeButton.setTitle("NEXT", for: .normal)
+        
+        switch currentColor {
+        case .red:
+            redSection.alpha = lightIsON
+            yellowSection.alpha = lightIsOff
+            greenSection.alpha = lightIsOff
+        case .yellow:
+            redSection.alpha = lightIsOff
+            yellowSection.alpha = lightIsON
+            greenSection.alpha = lightIsOff
+        case .green:
+            redSection.alpha = lightIsOff
+            yellowSection.alpha = lightIsOff
+            greenSection.alpha = lightIsON
+        }
+        
+    }
+    
 }
 
